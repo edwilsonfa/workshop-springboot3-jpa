a61703414ec122457c6924535011d1f6d06cb2b7
Ed Wilson <edwilsonfa@gmail.com>
2023-09-26 12:03:00 -0300

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -20,4 +20,10 @@ public class UserService {
         Optional<User> obj = repository.findById(Id);
         return obj.get();
     }
+
+    public User insert(User obj){
+       return repository.save(obj);
+    }
+
+
 }
